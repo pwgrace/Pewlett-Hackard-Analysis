76e4740916c5908d50954da225b9b60713bf1705
PRESTON GRACE IV <prestongraceiv@PRESTONs-MacBook-Air.local>
2020-10-20 14:34:18 -0500

    updating schema

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -45,15 +45,13 @@ CREATE TABLE titles (
 	PRIMARY KEY (emp_no, title, from_date)
 );
 
-CREATE TABLE Dept_Emp (
+CREATE TABLE dept_emp (
 	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 	PRIMARY KEY (dept_no, emp_no)
 	);
-	
-	SELECT * FROM departments;
